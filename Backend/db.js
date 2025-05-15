a5fccaefb375eaca81ef8a1b18427299724d63ac
Kevin5530 <kevin.pizarro5530@gmail.com>
2025-05-15 02:52:52 -0600

    implementación de Docker

--- Backend/db.js
+++ Backend/db.js
@@ -2,8 +2,8 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'Piz@rro5530',
-  database: process.env.DB_NAME || 'gestionrestaurantes',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || '',
   port: process.env.DB_PORT || 3306
 });
 db.connect(err => {
